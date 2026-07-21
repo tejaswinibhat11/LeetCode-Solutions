eebd8f85a7c2a286e4614d41668a41af3f733d28
tejaswinibhat11 <tejaswinibhat359@gmail.com>
2026-07-21 17:03:41 +0530

    added 1  more solutions

--- Arrays/0048_RotateImage.java
+++ Arrays/0048_RotateImage.java
@@ -9,6 +9,8 @@ class Solution {
                 int temp = matrix[i][j];
                 matrix[i][j] = matrix[j][i];
                 matrix[j][i] = temp;
+                // matrix[j][i] = temp;
+
             }
         }
 
